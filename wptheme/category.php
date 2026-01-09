6613f0c2bd8ee1b29b634c79a4898a90996374ce
KrishnaYamphu <krishnayamphu@gmail.com>
2026-01-09 18:04:52 +0545

    wp theme

--- wptheme/category.php
+++ wptheme/category.php
@@ -13,23 +13,33 @@ require "header.php"
         <hr>
 
         <?php
+        // $args = array(
+        //     'category'         => $category->cat_ID,
+        //     'orderby'     => 'date', // Order by date
+        //     'order'       => 'DESC', // Descending order
+        //     'post_type'   => 'post', // Can be 'page', 'custom_type', etc.
+        //     'post_status' => 'publish' // Post status
+        // );
+
         $args = array(
-            'category'         => $category->cat_ID,
-            'orderby'     => 'date', // Order by date
-            'order'       => 'DESC', // Descending order
-            'post_type'   => 'post', // Can be 'page', 'custom_type', etc.
-            'post_status' => 'publish' // Post status
+            'cat'         => $category_id,
+            'post_status' => 'publish'
         );
+        $posts = new WP_Query($args);
 
-        $posts_array = get_posts($args);
+        if ($posts->have_posts()) :
+            while ($posts->have_posts()) : $posts->the_post(); ?>
 
-        foreach ($posts_array as $post) : setup_postdata($post); ?>
-            <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-            <p><?php the_excerpt(); ?></p>
-        <?php endforeach;
-
-        wp_reset_postdata(); // Important: resets the post data to the original query
+                <h2><?php the_title(); ?></h2>
+                <p><?php the_excerpt(); ?></p>
+        <?php endwhile;
+            wp_reset_postdata();
+        else :
+            _e('Sorry, no posts were found.', 'textdomain');
+        endif;
         ?>
+
+        <?php echo paginate_links() ?>
     </div>
 </main>
 <?php require "footer.php" ?>
--- wptheme/header.php
+++ wptheme/header.php
@@ -17,43 +17,26 @@
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
             <div class="container">
                 <a class="navbar-brand" href="<?php echo get_bloginfo('url') ?>"><?php echo get_bloginfo('name') ?></a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#bs-primary-nav" aria-controls="bs-primary-nav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Link</a>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Dropdown
-                            </a>
-                            <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="#">Action</a></li>
-                                <li><a class="dropdown-item" href="#">Another action</a></li>
-                                <li>
-                                    <hr class="dropdown-divider">
-                                </li>
-                                <li><a class="dropdown-item" href="#">Something else here</a></li>
-                            </ul>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-                        </li>
-                    </ul>
-                    <form class="d-flex" role="search">
-                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                        <button class="btn btn-outline-success" type="submit">Search</button>
-                    </form>
-                </div>
+
+                <?php
+                wp_nav_menu(array(
+                    'theme_location'    => 'primary',
+                    'depth'             => 2,
+                    'container'         => 'div',
+                    'container_class'   => 'collapse navbar-collapse',
+                    'container_id'      => 'bs-primary-nav',
+                    'menu_class'        => 'nav navbar-nav',
+                    'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
+                    'walker'            => new WP_Bootstrap_Navwalker(),
+                ));
+                ?>
             </div>
         </nav>
 
-        <div class="contaienr">
+        <!-- <div class="contaienr">
             <?php wp_nav_menu(array('theme_location' => 'primary')); ?>
-        </div>
+        </div> -->
     </header>
